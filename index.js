f236057398d789fccafb7c9f4d44b308ba586ce6
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-04-23 14:27:24 +0100

    Added README.MD

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const handler = require("./lib/handler");
+
+yargs
+  .usage("$0 <cmd> [args]")
+  // .option("c", {
+  //   alias: "create",
+  //   demandOption: true,
+  //   default: true,
+  //   describe: "Create A New Mongo Schema",
+  //   type: "boolean"
+  // })
+  .demandOption(
+    ["create"],
+    `Please provide the <name> of the file you want the schema to be saved in.`
+  )
+  .command(
+    "$0 [create]",
+    "Mongo-scaffold - create a mongo schema",
+    yargs => {
+      yargs.positional("--create", {
+        type: "string",
+        alias: "c",
+        describe: "Name of the file where the schema would be stored."
+      });
+    },
+    (argv) => handler({
+      argv
+    })
+  )
+  .help().argv;
